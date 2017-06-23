ebdd8aa242531ab65125451619a56c759b4673f9
top-one-dev <toponedev@gmail.com>
2017-06-23 22:24:34 +0200

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :members
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :members do
+    root 'dashboards#index'
+  end
 end
