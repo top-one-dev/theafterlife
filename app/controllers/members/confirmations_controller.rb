60c88a77850d910d8f03b623b92ce80eaa280f1c
top-one-dev <toponedev@gmail.com>
2017-06-27 23:47:09 +0200

    Confirmation Flow updated

--- app/controllers/members/confirmations_controller.rb
+++ app/controllers/members/confirmations_controller.rb
@@ -1,2 +1,7 @@
-class Members::ConfirmationsController < ApplicationController
+class Members::ConfirmationsController < Devise::ConfirmationsController
+  private
+  def after_confirmation_path_for(resource_name, resource)
+    sign_in(resource)
+    members_root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  devise_for :members, :controllers => { :omniauth_callbacks => "members/omniauth_callbacks", :registrations => 'members/registrations' }
+  devise_for :members, :controllers => { :omniauth_callbacks => "members/omniauth_callbacks", :registrations => "members/registrations", :confirmations => "members/confirmations" }
 
   get 'health' => 'pages#health'
 
