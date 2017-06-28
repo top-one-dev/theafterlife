309b8683e43f2c2ee937785916ff5a106b9e96d7
top-one-dev <toponedev@gmail.com>
2017-06-28 13:43:31 +0200

    Reverse geocoding implemented

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,22 @@
 class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
 
-  geocoded_by :full_street_address
+  geocoded_by :address, :latitude  => :lat, :longitude => :lng
   after_validation :geocode
+
+  reverse_geocoded_by :lat, :lng do |address, results|
+    fields = [:street_number, :route, :neighborhood, :sublocality, :administrative_area_level_1, :administrative_area_level_2, :country, :postal_code]
+    result = results.first
+    if result
+      result.data['address_components'].each do |address_component|
+        fields.each do |field|
+          if address_component['types'].include? field.to_s
+            address[field] = address_component['long_name']
+          end
+        end
+      end
+    end
+  end
+
+  after_validation :reverse_geocode
 end
