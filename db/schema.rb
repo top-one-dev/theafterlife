85f48cb0af70a2325d941e12aa1d9dca9a8d842a
top-one-dev <toponedev@gmail.com>
2017-06-28 12:54:51 +0200

    Bundle update

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627122021) do
+ActiveRecord::Schema.define(version: 20170628103711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "addressable_id"
+    t.string "addressable_type"
+    t.string "name"
+    t.string "address"
+    t.string "street_number"
+    t.string "route"
+    t.string "neighborhood"
+    t.string "sublocality"
+    t.string "administrative_area_1"
+    t.string "administrative_area_2"
+    t.string "country"
+    t.string "postal_code"
+    t.decimal "lat", precision: 10, scale: 7
+    t.decimal "lng", precision: 10, scale: 7
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["addressable_type", "addressable_id"], name: "index_addresses_on_addressable_type_and_addressable_id"
+  end
 
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -43,6 +63,33 @@ ActiveRecord::Schema.define(version: 20170627122021) do
     t.index ["unlock_token"], name: "index_members_on_unlock_token", unique: true
   end
 
+  create_table "people", force: :cascade do |t|
+    t.bigint "member_id"
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.string "relation"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "index_people_on_member_id"
+  end
+
+  create_table "person_roles", force: :cascade do |t|
+    t.bigint "person_id"
+    t.bigint "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["person_id"], name: "index_person_roles_on_person_id"
+    t.index ["role_id"], name: "index_person_roles_on_role_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "social_accounts", force: :cascade do |t|
     t.bigint "member_id"
     t.string "provider"
@@ -52,5 +99,8 @@ ActiveRecord::Schema.define(version: 20170627122021) do
     t.index ["member_id"], name: "index_social_accounts_on_member_id"
   end
 
+  add_foreign_key "people", "members"
+  add_foreign_key "person_roles", "people"
+  add_foreign_key "person_roles", "roles"
   add_foreign_key "social_accounts", "members"
 end
