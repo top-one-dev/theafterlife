d21d6d18ee30a6722409c262c1ddca5c770e21fa
top-one-dev <toponedev@gmail.com>
2017-07-13 00:04:27 +0200

    Webhook basic function implemented

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -3,6 +3,30 @@ class WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def stripe
-    puts params.inspect
+    payload = request.body.read
+    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+    endpoint_secret = ENV['STRIPE_WEBHOOK_SECRET']
+    event = nil
+    begin
+      event = Stripe::Webhook.construct_event(payload, sig_header, endpoint_secret)
+    rescue JSON::ParserError => e
+      # Invalid payload
+      render :nothing => true, :status => 400
+      return
+    rescue Stripe::SignatureVerificationError => e
+      # Invalid signature
+      render :nothing => true, :status => 400
+      return
+    end
+
+    case params['type']
+    when 'invoice.payment_succeeded'
+      stripe_invoice_payment_succeeded
+    end
+    render :nothing => true, :status => 200
+  end
+
+  def stripe_invoice_payment_succeeded
+    puts 'stripe_invoice_payment_succeeded'
   end
 end
